a167dfcfac1ea2586aaab92e8a9d71b2bfbf5745
Dmytro Morozov <puny.flash@gmail.com>
2024-01-15 02:02:56 +0200

    Initial commit

---
+++ src/components/App.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { Page } from '@inertiajs/core'
+  import { ComponentResolver } from '../types'
+  import Render, { h } from './Render.svelte'
+  import store from '../store'
+
+  export const initialPage: Page | null = null
+  export const resolveComponent: ComponentResolver | null = null
+
+  $: child = $store.component && h($store.component.default, $store.page?.props)
+  $: layout = $store.component && $store.component.layout
+  $: components = layout
+    ? Array.isArray(layout)
+      ? layout
+          .concat(child)
+          .reverse()
+          .reduce((child, layout) => h(layout, $store.page?.props, [child]))
+      : h(layout, $store.page?.props, [child])
+    : child
+</script>
+
+<Render {...components} />
---
+++ src/components/SSR.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { Page } from '@inertiajs/core'
+  import App from './App.svelte'
+
+  export let id: string
+  export let initialPage: Page
+</script>
+
+<div data-server-rendered="true" {id} data-page={JSON.stringify(initialPage)}>
+  <App />
+</div>
